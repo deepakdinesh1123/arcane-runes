9355f84d2d0aac3f988ef1359dfdde9ac446a577
deepakdinesh1123 <d.deepakdinesh13@gmail.com>
2025-03-22 17:38:52 +0530

    go and rust templates added

---
+++ go/flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+    systems = [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" "aarch64-linux" ];
+    imports = [
+    ];
+    perSystem = { self', pkgs, lib, system, ... }: {
+      _module.args.pkgs = import inputs.nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+
+      devShells.default = with pkgs; mkShell {
+          buildInputs = [ go ];
+      };
+    };
+    flake = {
+    };
+  };
+}
